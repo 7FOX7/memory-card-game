7689afb52410eaac705bc24c3937744866f4925a
7FOX7 <kheeugene@gmail.com>
2024-07-14 22:07:00 -0400

    added code that allows to choose the difficulty (easy, medium, hard), shows random cards on each turn, and displays current round

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'; 
+import { useRef } from 'react'; 
 import { initialCards } from './data/data';
 import { GameOver } from './components/GameOver';
 import { ScoreBoard } from './components/ScoreBoard';
@@ -7,7 +8,13 @@ import { CardBoard } from './components/CardBoard';
 import shuffle from './functionality/shuffle';
 import './App.css'
 
+
+const maxCards__easyMode = 3; 
+const maxCards__mediumMode = 4; 
+const maxCards__hardMode = 5; 
+
 function App() {
+  const returnRef = useRef(null);
   const [playMode, setPlayMode] = useState(false); 
   const [cards, setCards] = useState(shuffle(initialCards)); 
   const [clickedCards, setClickedCards] = useState([]); 
@@ -15,15 +22,18 @@ function App() {
   const [bestScore, setBestScore] = useState(0); 
   const [gameShouldEnd, setGameShouldEnd] = useState(false); 
   const [gameIsLost, setGameIsLost] = useState(false);   
+  const currentRound = clickedCards.length;
+  const lastRound = cards.length + 3; 
   
   score > bestScore && setBestScore(score); 
   function handleClick(e) {
     const wasAlreadyClicked = clickedCards.find((clickedCard) => {
-      return clickedCard.id === e.currentTarget.id; 
+      return clickedCard === e.currentTarget.id; 
     })
+    // const clickedCardsCopy = [...clickedCards]; 
+    clickedCards.push(e.currentTarget.id);
 
-    clickedCards.push(e.currentTarget);
-    if(typeof(wasAlreadyClicked) !== "undefined" || clickedCards.length === cards.length) {
+    if(typeof(wasAlreadyClicked) !== "undefined" || currentRound + 1 === lastRound) {
       setGameShouldEnd(true)
       if(typeof(wasAlreadyClicked) !== "undefined") {
         setGameIsLost(true); 
@@ -31,23 +41,18 @@ function App() {
       }
     }
     else {
+      const updatedCards = getRandomizedCards(cards, clickedCards, initialCards); 
       setClickedCards(clickedCards);
-      setCards(shuffle(cards)); 
+      setCards(shuffle(updatedCards)); 
     }
     setScore(score + 1);
-    
-    // if(typeof(wasAlreadyClicked) !== "undefined") {
-    //   setGameShouldEnd(true); 
-    //   setGameIsLost(true); 
-    //   return; 
-    // }
-    // if(clickedCards.length === cards.length) {
-    //   setGameShouldEnd(true);
-    // }
-    
   }
 
-  function handleRestart() {
+  function handleRestart(e) {
+    if(e.currentTarget === returnRef.current) {
+      setPlayMode(false);
+      setBestScore(0); 
+    }
     setCards(shuffle(cards)); 
     setClickedCards([]); 
     setScore(0); 
@@ -55,12 +60,44 @@ function App() {
     setGameIsLost(false); 
   }
 
+  function startNewGame(e) {
+    if(e.currentTarget.id === "easyMode") {
+      const cards__easyMode = []; 
+      for(let i = 0; i < maxCards__easyMode; i++) {
+        // cards__easyMode.push(cards[Math.floor(Math.random() * cards.length)]); 
+        const randomCard = getRandomCard(cards__easyMode, initialCards); 
+        cards__easyMode.push(randomCard); 
+      }
+      setCards(shuffle(cards__easyMode)); 
+    }
+
+    else if(e.currentTarget.id === "mediumMode") {
+      const cards__mediumMode = []; 
+      for(let i = 0; i < maxCards__mediumMode; i++) {
+        const randomCard = getRandomCard(cards__mediumMode, initialCards); 
+        cards__mediumMode.push(randomCard); 
+      }
+      setCards(shuffle(cards__mediumMode)); 
+    }
+
+    else if(e.currentTarget.id === "hardMode") {
+      const cards__hardMode = []; 
+      for(let i = 0; i < maxCards__hardMode; i++) {
+        const randomCard = getRandomCard(cards__hardMode, initialCards); 
+        cards__hardMode.push(randomCard); 
+      }
+      setCards(shuffle(cards__hardMode)); 
+    }
+
+    setPlayMode(true); 
+  }
+
   const starting = (
     <>
       <p>This is a starting page!</p>
-      <button onClick={() => setPlayMode(true)}>Easy</button>
-      <button onClick={() => setPlayMode(true)}>Medium</button>
-      <button onClick={() => setPlayMode(true)}>Hard</button>
+      <button id="easyMode" onClick={startNewGame}>Easy</button>
+      <button id="mediumMode" onClick={startNewGame}>Medium</button>
+      <button id="hardMode" onClick={startNewGame}>Hard</button>
     </>
   )
 
@@ -78,11 +115,53 @@ function App() {
           />
         })}
       </CardBoard>
+      <p>{currentRound}/{lastRound}</p>
+      <button ref={returnRef} onClick={handleRestart}>Return to main menu</button>
     </>
   )   
 
   return playMode ? playground : starting
 }
 
+
+function getRandomCard(arr, sourceArray) {
+  const randomCard = sourceArray[Math.floor(Math.random() * sourceArray.length)]; 
+  if(!arr.includes(randomCard)) {
+    return randomCard; 
+  } 
+  return getRandomCard(arr, sourceArray)
+}
+
+function getRandomizedCards(cards, clickedCards, initialCards) { 
+  const nonClickedCards = initialCards.filter((card) => {
+    const cardId = card.id; 
+    return !clickedCards.includes(cardId); 
+  }); 
+  const randomCard = getRandomCard(cards, nonClickedCards); 
+  const cardsCopy = [...cards]; 
+
+  // cardCopy.shift() - returns an element. that is why we call differently
+  cardsCopy.shift(); 
+  const randomizedCards = nonClickedCards.length === 0 ? cards : [...cardsCopy, randomCard]; 
+
+  return randomizedCards
+} 
+
+
+// lets assume these are not clicked cards: 
+
+// sample card 1
+// sample card 5
+// sample card 9
+
+// this is the current array of cards randomly selected: 
+
+// sample card 2
+// sample card 5
+// sample card 3
+// sample card 4
+
+// randomCard = getRandomCard(nonClickedCard, cards)
+// randomizedCards = [...cards, randomCard]
 
 export default App
--- src/data/data.js
+++ src/data/data.js
@@ -14,5 +14,40 @@ export const initialCards = [
     {
         id: nanoid(8), 
         content: "Sample content 3"
+    }, 
+
+    {
+        id: nanoid(8), 
+        content: "Sample content 4"
+    }, 
+
+    {
+        id: nanoid(8), 
+        content: "Sample content 5"
+    }, 
+
+    {
+        id: nanoid(8), 
+        content: "Sample content 6"
+    }, 
+
+    {
+        id: nanoid(8), 
+        content: "Sample content 7"
+    }, 
+
+    {
+        id: nanoid(8), 
+        content: "Sample content 8"
+    }, 
+
+    {
+        id: nanoid(8), 
+        content: "Sample content 9"
+    }, 
+    
+    {
+        id: nanoid(8), 
+        content: "Sample content 10"
     }
 ]
